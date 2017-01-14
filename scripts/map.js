9169c8440841d720591d8831f5085e0af78d58e2
Jesse Millar <hellojessemillar@gmail.com>
2017-01-14 15:46:40 -0700

    Slowly getting there with the map data structure

--- scripts/map.js
+++ scripts/map.js
@@ -1,10 +1,5 @@
 var map_size = 25;
 var map = [];
-var grid = new PF.Grid(map_size, map_size); 
-var finder = new PF.AStarFinder();
-var path = finder.findPath(1, 2, 4, 2, grid);
-
-console.log(path);
 
 // https://en.wikipedia.org/wiki/Box_Drawing
 var tiles = {
@@ -33,11 +28,26 @@ var tiles = {
 var characters = [tiles.ns, tiles.ew, tiles.nesw, tiles.ne, tiles.es, tiles.sw, tiles.wn, tiles.nes, tiles.esw, tiles.swn, tiles.wne, tiles.treasure, tiles.city, tiles.portal, tiles.gray];
 
 function generate_map(callback) {
-	for (y=0;y<map_size;y++) {
-		for (x=0;x<map_size;x++) {
-			var i = Math.floor(Math.random() * characters.length)
-			map.push(characters[i]);
+	make_empty_map(function() {
+		for (y=0;y<map_size;y++) {
+			for (x=0;x<map_size;x++) {
+				if (probability(10)) {
+					var i = Math.floor(Math.random() * characters.length)
+					map[y][x]=characters[i];
+				}
+			}
 		}
+	
+		if (callback) {
+			callback();
+		}
+	});
+}
+
+// Make an empty 2D array of size map_size
+function make_empty_map(callback) {
+	for (y=0;y<map_size;y++) {
+		map.push(new Array(map_size));
 	}
 
 	if (callback) {
@@ -45,8 +55,34 @@ function generate_map(callback) {
 	}
 }
 
-function draw_map() {
-	for (i=0;i<map_size;i++) {
-		console.log(map.slice(i*map_size, i*map_size+map_size).join(""));
+function find_path(callback) {
+	var grid = new PF.Grid(map_size, map_size); 
+	var finder = new PF.AStarFinder();
+	var path = finder.findPath(1, 2, 4, 2, grid);
+
+	console.log(path);
+
+	if (callback) {
+		callback();
 	}
+}
+
+function draw_map() {
+	console.log(map);
+
+	/*
+	for (y=0;y<map_size;y++) {
+		for (x=0;x<map[y].length;x++) {
+			console.log(map[y][x]);
+		}
+	}
+	*/
+}
+
+function probability(percent) {
+	if (Math.random() * 100 < percent) {
+		return true
+	}
+
+	return false
 }
