a14d55728970b1cff5e558147d68ade16e456da4
Jesse Millar <hellojessemillar@gmail.com>
2017-04-16 08:48:09 -0600

    Trying to get the database to talk

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 var app = express();
+
+var MongoClient = require("mongodb").MongoClient;
+var Server = require("mongodb").Server;
 
 app.use(cors());
 
 app.listen(8000, function() {
-	console.log('Listening on 8000');
-})
+	console.log("Listening on 8000");
+});
 
 // Serve static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.post('/users', function(req, res) {
-	console.log('Hellooooooooooooooooo!');
-})
+MongoClient.connect("mongodb://localhost:27017/venturerealm", {
+	native_parser: true
+}, function(err, db) {
+	// assert.equal(null, err);
+
+	db.collection("map").update({
+		a: 1
+	}, {
+		b: 1
+	}, {
+		upsert: true
+	}, function(err, result) {
+		// assert.equal(null, err);
+		// assert.equal(1, result);
+
+		db.close();
+	});
+});
