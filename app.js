5af6825cc51d089b3ce0205d8ff0183591f6c045
Jesse Millar <hellojessemillar@gmail.com>
2017-04-17 14:46:28 -0600

    Fixing some routes

--- app.js
+++ app.js
@@ -62,7 +62,6 @@ app.use(session({
 app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, "public")));
 
 connectToMongo();
 
--- routes/game.js
+++ routes/game.js
@@ -3,7 +3,7 @@ var passport = require("passport");
 var ensureLoggedIn = require("connect-ensure-login").ensureLoggedIn();
 var router = express.Router();
 
-router.get("/", ensureLoggedIn, function(req, res, next) {
+router.get("/game", ensureLoggedIn, function(req, res, next) {
 	res.render("game", {
 		user: req.user
 	});
